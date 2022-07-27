f7fd84191ecbb5b52b237560286ab66304d69e1c
System Administrator <gokarna.adhikari14@gmail.com>
2022-07-27 12:13:33 +0545

    Need to fix minor bug in back propagation

--- backPropagation.py
+++ backPropagation.py
@@ -1,7 +1,8 @@
 from cmath import e
+from http.client import FOUND
 import random
 
-# OR gate
+# For OR gate
 tTable = [
     [0, 0, 0],
     [0, 1, 1],
@@ -13,15 +14,15 @@ def sigmoid(x):
     return 1/(1 + e**(-x))
         
 
-def summation(xs, ws, b):
+def summation(xs, ws, b=0):
     res = 0
     for x, w in zip(xs, ws):
         res += x*w
-    return res
+    return res + b
  
 
 w, b = [random.randint(1, 5)/5 for i in range(6)], [random.randint(1, 5)/5 for i in range(2)]
-a, epoch = 0.2, 1
+a, epoch = 0.2, 5050
 threshold = 0.000000001
 f_in, f_out = [], []
 
@@ -39,13 +40,38 @@ for i in range(epoch):
         f_in.append(summation(row[:-1], w[2:4], b[0]))
         for fin in f_in:
             f_out.append(sigmoid(fin))
-        f_in.append(summation(f_out, w[4:5], b[1]))
+        f_in.append(summation(f_out, w[4:6], b[1]))
         f_out.append(sigmoid(f_in[-1]))
         
-        error = ((row[-1] - sigmoid(f_out[-1]))**2)/2
-        delta = abs(f_in[-1] - f_out[-1])
+        delta = {}
+        delta['f2'] = f_out[-1]*(1-f_out[-1])*(row[-1]-f_out[-1]) 
+        delta['f0'] = f_out[0]*(1-f_out[0])*w[4]*delta['f2']
+        delta['f1'] = f_out[1]*(1-f_out[1])*w[5]*delta['f2']
         
-        print(f'Fin: {f_in}\nFout: {f_out}\nDelta: {delta}\nError: {error}\n')
+        
+        # updating weights
+        w = []
+        for wIndex in range(6):
+            deltaIndex = "f" + str(int(wIndex/2))
+            if wIndex < 4:
+                delW = a*delta[deltaIndex]*row[int(wIndex/2)]
+            else:
+                delW = a*delta[deltaIndex]*f_out[wIndex-4]
+            w.append(w_old[wIndex] + delW)
+    error = 0
+    print(f'w: {w_old}\nFin: {f_in}\nFout: {f_out}\nDelta: {delta}\nw_new: {w}\nError: {error}\n')
     
     
+print("Prediction:")
+
+for row in tTable:
+    f_in, f_out = [], []
+    f_in.append(summation(row[:-1], w[0:2], b[0]))
+    f_in.append(summation(row[:-1], w[2:4], b[0]))
+    for fin in f_in:
+        f_out.append(sigmoid(fin))
+    f_in.append(summation(f_out, w[4:6], b[1]))
+    f_out.append(sigmoid(f_in[-1]))
+    
+    print(f'\t{f_out[-1]}')
     
